8b3681a50cc499a122021e2c697932342f9e9f36
ursu96 <ursualexandru96@gmail.com>
2019-01-20 21:05:05 +0200

    save

--- src/main/java/com/example/springtutorialsapp/main/SpringTutorialsAppApplication.java
+++ src/main/java/com/example/springtutorialsapp/main/SpringTutorialsAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.springtutorialsapp;
+package com.example.springtutorialsapp.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
