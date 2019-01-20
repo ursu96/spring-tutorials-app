dd1569293a960b869ddb2f5b505be42301831602
ursu96 <ursualexandru96@gmail.com>
2019-01-20 21:07:27 +0200

    save

--- src/main/java/com/example/springtutorialsapp/controllers/HelloController.java
+++ src/main/java/com/example/springtutorialsapp/controllers/HelloController.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
-
+    
     @RequestMapping("hello")
     public String sayHi(){
         return "Hil";
