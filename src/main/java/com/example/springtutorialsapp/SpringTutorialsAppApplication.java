7745f395a608601725f45631d65b07bd24126f8b
ursu96 <ursualexandru96@gmail.com>
2019-01-20 21:00:55 +0200

    test

---
+++ src/main/java/com/example/springtutorialsapp/SpringTutorialsAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.springtutorialsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorialsAppApplication.class, args);
+    }
+
+}
+
