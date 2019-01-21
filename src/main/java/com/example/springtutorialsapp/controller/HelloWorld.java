e815ad5c64bdd58d94b109d639bfaf988bc45009
ursu96 <ursualexandru96@gmail.com>
2019-01-21 12:42:32 +0200

    save

--- src/main/java/com/example/springtutorialsapp/controller/HelloWorld.java
+++ src/main/java/com/example/springtutorialsapp/controller/HelloWorld.java
@@ -1,14 +1,15 @@
 package com.example.springtutorialsapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
-    @RequestMapping("/hello")
-    public String afiseaza(){
+    @RequestMapping("/")
+    public String getWork() {
         return "Hi";
     }
 
